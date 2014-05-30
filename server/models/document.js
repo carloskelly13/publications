c6b3fd86c61b43c48a171027e655c3c05b78cc7d
Carlos Paelinck <carlos.paelinck@osgcorp.com>
2014-05-30 13:56:56 -0400

    Updated PDFKit npm module. Cleaned up PDF code

--- server/models/document.js
+++ server/models/document.js
@@ -53,18 +53,24 @@ documentSchema.methods = {
             (shape.height / 2.0)  * dpi);
         }
 
-        obj.lineWidth(shape.strokeWidth)
-          .fillColor(shape.fill, shape.fillOpacity)
-          .strokeColor(shape.stroke, shape.strokeOpacity);
-
         if (shape.fillOpacity > 0 && shape.strokeWidth === 0) {
-          obj.fill();
+          obj.fillColor(shape.fill, shape.fillOpacity)
+            .fill();
 
         } else if (shape.fillOpacity === 0 && shape.strokeWidth > 0) {
-          obj.stroke();
+          obj.strokeColor(shape.stroke, shape.strokeOpacity)
+            .lineWidth(shape.strokeWidth)
+            .lineCap('square')
+            .lineJoin('miter')
+            .stroke();
 
         } else if (shape.fillOpacity > 0 && shape.strokeWidth > 0) {
-          obj.fillAndStroke();
+          obj.fillColor(shape.fill, shape.fillOpacity)
+            .strokeColor(shape.stroke, shape.strokeOpacity)
+            .lineWidth(shape.strokeWidth)
+            .lineCap('square')
+            .lineJoin('miter')
+            .fillAndStroke();
         }
 
       } else if (shape.type === 'text') {
