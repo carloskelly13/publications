15df3d995d8177651d0806e26f000b85bebd1ce4
Carlos Paelinck <carlos@carlospaelinck.com>
2015-03-06 08:46:42 -0700

    Remove dubugger; statement from auth controller.

--- client/js/src/security/authentication.js
+++ client/js/src/security/authentication.js
@@ -11,7 +11,6 @@
 
         var authentication = {
           requestSecurityContext: function() {
-            debugger;
             if (securityContext.authenticated) {
               return $q.when(securityContext);
 
