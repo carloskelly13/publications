371de65fb93a8513194305fffe066c56b20a58de
Carlos Paelinck <carlos@carlospaelinck.com>
2016-08-14 09:42:52 -0500

    Continued styling.

--- app/js/components/document/document.loadingNavbar.jsx
+++ app/js/components/document/document.loadingNavbar.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import NProgress from 'nprogress'
-import AboutButton from '../ui/about.button'
 
 export default class DocumentLoadingNavbar extends Component {
 
@@ -13,10 +12,6 @@ export default class DocumentLoadingNavbar extends Component {
   }
 
   render() {
-    return <div className="navbar-container">
-      <div className="navbar-controls-left">
-        <AboutButton />
-      </div>
-    </div>
+    return <div className="toolbar"></div>
   }
 }
--- app/js/components/document/document.navbar.jsx
+++ app/js/components/document/document.navbar.jsx
@@ -18,41 +18,81 @@ export default class DocumentNavbar extends Component {
     return (
       <div className="toolbar">
         <div className="scroll-view">
-          <button className="button" onClick={this.props.save}>Save</button>
+          <button
+            className="button"
+            onClick={this.props.save}
+          >
+            Save
+          </button>
           <a
             className="button"
             href={ `${Urls.ApiBase}/documents/${this.props.doc.id}/pdf` }
-            target="_blank">
+            target="_blank"
+          >
             PDF
           </a>
-          <button className="button" onClick={this.props.viewAllDocuments}>All Documents</button>
+          <button
+            className="button"
+            onClick={this.props.viewAllDocuments}
+          >
+            All Documents
+          </button>
           <div className="spacer"></div>
-          <button className="button button-icon" onClick={this.zoomIn}>
+          <button
+            className="button button-icon"
+            onClick={this.zoomIn}
+          >
             <span className="icon icon-zoom-in"></span>
           </button>
-          <span className="button-label">{this.props.zoom * 100}%</span>
-          <button className="button button-icon" onClick={this.zoomOut}>
+          <span className="button-label button-label-bordered">
+            {this.props.zoom * 100}%
+          </span>
+          <button
+            className="button button-icon"
+            onClick={this.zoomOut}
+          >
             <span className="icon icon-zoom-out"></span>
           </button>
           <div className="spacer"></div>
-          <button disabled={!this.props.selectedShape} className="button button-icon" onClick={this.props.cutShape}>
+          <button
+            disabled={!this.props.selectedShape}
+            className="button button-icon"
+            onClick={this.props.cutShape}
+          >
             <span className="icon icon-cut"></span>
           </button>
-          <button disabled={!this.props.selectedShape} className="button button-icon" onClick={this.props.copyShape}>
+          <button
+            disabled={!this.props.selectedShape}
+            className="button button-icon"
+            onClick={this.props.copyShape}
+          >
             <span className="icon icon-copy"></span>
           </button>
-          <button disabled={!this.props.clipboard} className="button button-icon" onClick={this.props.pasteShape}>
+          <button
+            disabled={!this.props.clipboard}
+            className="button button-icon"
+            onClick={this.props.pasteShape}
+          >
             <span className="icon icon-paste"></span>
           </button>
-          <button disabled={!this.props.selectedShape} className="button button-icon" onClick={this.props.deleteShape}>
+          <button
+            disabled={!this.props.selectedShape}
+            className="button button-icon"
+            onClick={this.props.deleteShape}
+          >
             <span className="icon icon-delete"></span>
           </button>
           <div className="spacer"></div>
-          <button className="button button-icon button--inspector" onClick={this.props.toggleInspector}>
-            <span className={`icon icon-inspector ${this.props.showInspector ? 'active' : ''}`}></span>
+          <button
+            className="button button-icon button--inspector"
+            onClick={this.props.toggleInspector}
+          >
+            <span
+              className={`icon icon-inspector ${this.props.showInspector ? 'active' : ''}`}>
+            </span>
           </button>
         </div>
       </div>
-    );
+    )
   }
 }
