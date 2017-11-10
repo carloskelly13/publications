353c341339733176fcff56fc8f2bc488be2ceb51
Carlos Paelinck <carlos@carlospaelinck.com>
2017-11-09 20:24:16 -0600

    Switch back logo

--- app/client/components/toolbar/index.js
+++ app/client/components/toolbar/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
+import styled from "styled-components"
 import { Toolbar as ToolbarBase } from "../ui/toolbar"
 import FileMenu from "./file"
 import NewShapeMenu from "./new-shape"
@@ -11,7 +12,14 @@ import { ContentContainer } from "../ui/containers"
 import { currentDocumentSelector, selectedShapeSelector } from "../../modules/document"
 import { currentUserSelector } from "../../modules/session"
 import { sidePanelVisibleSelector, toggleSidePanel } from "../../modules/ui"
-import PubLogo from "./pub-logo"
+import { AppColors } from "../../util/constants"
+
+const Header = styled.span`
+  color: ${AppColors.Highlight};
+  font-weight: 800;
+  font-size: 0.95em;
+  margin: 0 0.65em 0 0;
+`
 
 const Toolbar = props => {
   const {
@@ -23,7 +31,7 @@ const Toolbar = props => {
   return (
     <ToolbarBase>
       <ContentContainer verticalAlign>
-        <PubLogo />
+        <Header>Publications</Header>
         <FileMenu disabled={!user} />
         <EditMenu disabled={!currentDocument} />
         <NewShapeMenu disabled={!currentDocument} />
