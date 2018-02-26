977aaba5865786e4c4ddb8d39c5e5b8128051d8a
Carlos Paelinck <carlos@carlospaelinck.com>
2018-02-26 15:18:15 -0600

    Disable BundleAnalyzerPlugin by default.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer");
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer");
 
 module.exports = {
   entry: {
@@ -55,7 +55,7 @@ module.exports = {
       template: "./src/index.html",
       inject: "body",
     }),
-    new BundleAnalyzerPlugin.BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin.BundleAnalyzerPlugin(),
   ],
 
   node: {
